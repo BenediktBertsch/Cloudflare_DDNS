8019c6781b4970dad293e4e56d383ff8b3fb87ce
Benedikt Bertsch <benedikt.bertsch@gmail.com>
2019-08-11 17:32:49 +0200

    ipv6 support added

--- index.js
+++ index.js
@@ -1,17 +1,23 @@
+//
+//Easy A Record Update Script for Cloudflare written by Benedikt Bertsch
+//
 //Libraries
 const request = require('request');
 //Docker variables
+//IPv4
 api_token = process.env.token;
 mail_address = process.env.mail;
 zone_identifier = process.env.zone;
 domain = process.env.domain;
-proxied = process.env.proxied;  //True od. False
+proxied = process.env.proxied;
 intervalmin = process.env.interval;
+//IPv6
+ipv6active = process.env.ipv6activate;
 //Update DNS Entry
 setInterval(() => {
     var options = {
         method: 'GET',
-        url: 'https://api.ipify.org'
+        url: 'https://v4.ident.me/'
     }
     request(options, function (error, response, body) {
         if (error) throw new Error(error);
@@ -57,10 +63,48 @@ setInterval(() => {
                         request(options, function (error, response, body) {
                             if (error) throw new Error(error);
                             console.log(body);
+                            console.log('A Record Updated...')
+                        })
+                    }
+                }
+                else if(ipv6active == true){
+                    if(dnsentry.content == 'AAAA'){
+                        var options = {
+                            method: 'GET',
+                            url: 'https://v6.ident.me/'
+                        }
+                        request(options, function (error, response, body) {
+                            if (error) throw new Error(error);
+                            ipv6 = body;
+                            options = {
+                                method: 'PUT',
+                                url: 'https://api.cloudflare.com/client/v4/zones/' + zone_identifier + '/dns_records/' + dnsentry.id,
+                                headers:
+                                {
+                                    Host: 'api.cloudflare.com',
+                                    'X-Auth-Key': api_token,
+                                    'X-Auth-Email': mail_address,
+                                    'Content-Type': 'application/json'
+                                },
+                                body:
+                                {
+                                    type: 'AAAA',
+                                    name: domain,
+                                    content: ipv6,
+                                    ttl: intervalmin * 60,
+                                    proxied: proxied
+                                },
+                                json: true
+                            };
+                            request(options, function (error, response, body) {
+                                if (error) throw new Error(error);
+                                console.log(body);
+                                console.log('AAAA Record Updated...')
+                            })
                         })
                     }
                 }
             })
         });
     });
-}, intervalmin * 1000);
+}, intervalmin * 1000 * 60);
